1e91c9dec5d3a6609bddea2ff1452c265a2fd0c0
dobe4ever <132445266+dobe4ever@users.noreply.github.com>
2023-10-31 04:57:50 +0000

    initial

--- bot.py
+++ bot.py
@@ -1,16 +1,30 @@
-from flask import Flask
+import os
+
+import openai
+from flask import Flask, render_template, request
+
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return open("index.html", "r")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
-# def run():
-#   app.run(host='0.0.0.0', port=8080)
+@app.route("/get_response")
+def get_response():
+    message = request.args.get("message")
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message}],
+    )
+    response = completion["choices"][0]["message"]["content"]
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+
 
