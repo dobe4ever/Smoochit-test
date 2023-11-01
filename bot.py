359cd38656bc72129890f9b6c6880cbc6f2d0b6b
dobe4ever <132445266+dobe4ever@users.noreply.github.com>
2023-11-01 20:53:11 +0000

    sticky bottom

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ def get_response():
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=context,
-        max_tokens=100
+        max_tokens=1000
     )
     response = completion["choices"][0]["message"]["content"]
     
